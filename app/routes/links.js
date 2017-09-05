e44bf430c222a9c97a6002200db6bc69454857f5
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-05 16:37:18 -0600

    more lists

--- app/routes/links.js
+++ app/routes/links.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      goodnewsarchive: this.get('store').findAll('goodnewsarchive')
+    })
+  }
 });
--- app/routes/ministries.js
+++ app/routes/ministries.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      ministry: this.get('store').findAll('ministry'),
+      communityoutreach: this.get('store').findAll('communityoutreach')
+    })
+  }
 });
