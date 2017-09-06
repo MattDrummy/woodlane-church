feeeb0e1d11275f48aa02636e13bfa79f60be80a
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-06 13:01:56 -0600

    removed inflector

--- app/routes/staff.js
+++ app/routes/staff.js
@@ -2,15 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    const rules = {
-      plurals: [
-        [ /$/, '-ember']
-      ],
-      singular: [
-        [ /\s$/, '']
-      ]
-    }
-    const inflector = new Ember.Inflector(rules);
     return Ember.RSVP.hash({
       officestaff: this.get('store').findAll('officestaff'),
       sundayschoolstaff: this.get('store').findAll('sundayschoolstaff'),
